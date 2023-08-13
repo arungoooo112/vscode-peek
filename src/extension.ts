9d02796dfabd04cce3822b36aa2fdad6fdb1d9b0
frankrun <arungoooo@163.com>
2023-08-13 22:55:00 +0800

    modified:   package.json 	modified:   src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,166 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+class PeekFileBaseProvider {
+  protected locations: vscode.Location[];
+  protected filePatterns: vscode.GlobPattern[];
+  protected matchPattern: RegExp;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld" is now active!');
+  constructor(filePatterns: vscode.GlobPattern[], matchPattern: string) {
+    this.locations = [];
+    this.filePatterns = filePatterns;
+    this.matchPattern = new RegExp(matchPattern);
+  }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from !');
-	});
+  protected async handleFile(file: vscode.Uri, word: string) {
+    try {
+      const fileContentBytes = await vscode.workspace.fs.readFile(file);
+      const fileContent = Buffer.from(fileContentBytes).toString("utf8");
+      if (this.matchPattern.test(fileContent)) {
+        this.locations.push(
+          new vscode.Location(file, new vscode.Position(0, 0))
+        );
+      }
+    } catch (error) {
+      console.error(`Error reading file: ${file.fsPath}`, error);
+    }
+  }
 
-	context.subscriptions.push(disposable);
+  protected async traverseFiles(word: string) {
+    const files = await this.findFiles();
+    for await (const file of this.iterateFiles(files, word)) {
+      await this.handleFile(file, word);
+    }
+  }
+
+  private async findFiles(): Promise<vscode.Uri[]> {
+    const patternFilesPromises = this.filePatterns.map((pattern) =>
+      vscode.workspace.findFiles(pattern)
+    );
+    const patternFilesArrays = await Promise.all(patternFilesPromises);
+    return patternFilesArrays.flat();
+  }
+
+  private async *iterateFiles(
+    files: vscode.Uri[],
+    word: string
+  ): AsyncIterableIterator<vscode.Uri> {
+    for (const file of files) {
+      yield file;
+    }
+  }
 }
 
-// This method is called when your extension is deactivated
+class PeekFileDefinitionProvider
+  extends PeekFileBaseProvider
+  implements vscode.DefinitionProvider
+{
+  async provideDefinition(
+    document: vscode.TextDocument,
+    position: vscode.Position
+  ): Promise<vscode.Location[] | undefined> {
+    const word = document.getText(document.getWordRangeAtPosition(position));
+    this.matchPattern = new RegExp(
+      this.matchPattern.source.replace(/{word}/g, word)
+    );
+    await this.traverseFiles(word);
+    return this.locations;
+  }
+}
+
+class PeekFileImplementationProvider
+  extends PeekFileBaseProvider
+  implements vscode.ImplementationProvider
+{
+  async provideImplementation(
+    document: vscode.TextDocument,
+    position: vscode.Position
+  ): Promise<vscode.Location[] | undefined> {
+    const word = document.getText(document.getWordRangeAtPosition(position));
+    this.matchPattern = new RegExp(
+      this.matchPattern.source.replace(/{word}/g, word)
+    );
+    await this.traverseFiles(word);
+    return this.locations;
+  }
+}
+
+class PeekFileDeclarationProvider
+  extends PeekFileBaseProvider
+  implements vscode.DeclarationProvider
+{
+  async provideDeclaration(
+    document: vscode.TextDocument,
+    position: vscode.Position
+  ): Promise<
+    vscode.Location | vscode.Location[] | vscode.LocationLink[] | undefined
+  > {
+    const word = document.getText(document.getWordRangeAtPosition(position));
+    this.matchPattern = new RegExp(
+      this.matchPattern.source.replace(/{word}/g, word)
+    );
+    await this.traverseFiles(word);
+    return this.locations;
+  }
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  const config = vscode.workspace.getConfiguration("vscode_peek");
+  const activeLanguages = config.get("activeLanguages") as string[];
+  const definitionFilePatterns = config.get(
+    "definitionFilePatterns"
+  ) as vscode.GlobPattern[];
+  const definitionMatchPattern = config.get("definitionMatchPattern") as string;
+
+  const implementationFilePatterns = config.get(
+    "ImplementationFilePatterns"
+  ) as vscode.GlobPattern[];
+  const implementationMatchPattern = config.get(
+    "ImplementationMatchPattern"
+  ) as string;
+
+  const declarationFilePatterns = config.get(
+    "declarationFilePatterns"
+  ) as vscode.GlobPattern[];
+  const declarationMatchPattern = config.get(
+    "declarationMatchPattern"
+  ) as string;
+
+  const peekFilter: vscode.DocumentFilter[] = activeLanguages.map(
+    (language) => ({
+      language,
+      scheme: "file",
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.languages.registerDefinitionProvider(
+      peekFilter,
+      new PeekFileDefinitionProvider(
+        definitionFilePatterns,
+        definitionMatchPattern
+      )
+    )
+  );
+
+  context.subscriptions.push(
+    vscode.languages.registerImplementationProvider(
+      peekFilter,
+      new PeekFileImplementationProvider(
+        implementationFilePatterns,
+        implementationMatchPattern
+      )
+    )
+  );
+
+  context.subscriptions.push(
+    vscode.languages.registerDeclarationProvider(
+      peekFilter,
+      new PeekFileDeclarationProvider(
+        declarationFilePatterns,
+        declarationMatchPattern
+      )
+    )
+  );
+}
+
 export function deactivate() {}
