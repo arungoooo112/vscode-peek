2d622fc5a0f214a8434f3f0090e71c942c4d4ede
W_Frank <61907503+arungoooo112@users.noreply.github.com>
2023-08-22 22:49:38 +0800

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,70 +1,112 @@
 import * as vscode from "vscode";
 import * as provider from './provider';
 
+let definitionProvider: provider.PeekFileDefinitionProvider | undefined;
+let implementationProvider: provider.PeekFileImplementationProvider | undefined;
+let declarationProvider: provider.PeekFileDeclarationProvider | undefined;
+
 export function activate(context: vscode.ExtensionContext) {
   const config = vscode.workspace.getConfiguration("vscode_peek");
   const activeLanguages = config.get("activeLanguages") as string[];
 
-  const definitionFilePatterns = config.get(
-    "definitionFilePatterns"
-  ) as vscode.GlobPattern[];
-  const definitionMatchPattern = config.get(
-    "definitionMatchPattern") as string;
+  function registerProviders() {
+    if (definitionProvider) {
+      definitionProvider.dispose();
+    }
+    if (implementationProvider) {
+      implementationProvider.dispose();
+    }
+    if (declarationProvider) {
+      declarationProvider.dispose();
+    }
 
-  const implementationFilePatterns = config.get(
-    "ImplementationFilePatterns"
-  ) as vscode.GlobPattern[];
-  const implementationMatchPattern = config.get(
-    "ImplementationMatchPattern"
-  ) as string;
+    const definitionFilePatterns = config.get(
+      "definitionFilePatterns"
+    ) as vscode.GlobPattern[];
+    const definitionMatchPattern = config.get(
+      "definitionMatchPattern"
+    ) as string;
 
-  const declarationFilePatterns = config.get(
-    "declarationFilePatterns"
-  ) as vscode.GlobPattern[];
-  const declarationMatchPattern = config.get(
-    "declarationMatchPattern"
-  ) as string;
+    const implementationFilePatterns = config.get(
+      "ImplementationFilePatterns"
+    ) as vscode.GlobPattern[];
+    const implementationMatchPattern = config.get(
+      "ImplementationMatchPattern"
+    ) as string;
 
-  const peekFilter: vscode.DocumentFilter[] = activeLanguages.map(
-    (language) => ({
-      language,
-      scheme: "file",
+    const declarationFilePatterns = config.get(
+      "declarationFilePatterns"
+    ) as vscode.GlobPattern[];
+    const declarationMatchPattern = config.get(
+      "declarationMatchPattern"
+    ) as string;
+
+    const peekFilter: vscode.DocumentFilter[] = activeLanguages.map(
+      (language) => ({
+        language,
+        scheme: "file",
+      })
+    );
+
+    definitionProvider = new provider.PeekFileDefinitionProvider(
+      definitionFilePatterns,
+      definitionMatchPattern
+    );
+    implementationProvider = new provider.PeekFileImplementationProvider(
+      implementationFilePatterns,
+      implementationMatchPattern
+    );
+    declarationProvider = new provider.PeekFileDeclarationProvider(
+      declarationFilePatterns,
+      declarationMatchPattern
+    );
+
+    context.subscriptions.push(
+      vscode.languages.registerDefinitionProvider(
+        peekFilter,
+        definitionProvider
+      )
+    );
+
+    context.subscriptions.push(
+      vscode.languages.registerImplementationProvider(
+        peekFilter,
+        implementationProvider
+      )
+    );
+
+    context.subscriptions.push(
+      vscode.languages.registerDeclarationProvider(
+        peekFilter,
+        declarationProvider
+      )
+    );
+  }
+
+  registerProviders();
+
+  // Listen for configuration changes
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeConfiguration((event) => {
+      if (event.affectsConfiguration("vscode_peek")) {
+        config.update().then(registerProviders);
+      }
     })
-  );
-
-  context.subscriptions.push(
-    vscode.languages.registerDefinitionProvider(
-      peekFilter,
-      new provider.PeekFileDefinitionProvider(
-        definitionFilePatterns,
-        definitionMatchPattern
-      )
-    )
-  );
-
-  context.subscriptions.push(
-    vscode.languages.registerImplementationProvider(
-      peekFilter,
-      new provider.PeekFileImplementationProvider(
-        implementationFilePatterns,
-        implementationMatchPattern
-      )
-    )
-  );
-
-  context.subscriptions.push(
-    vscode.languages.registerDeclarationProvider(
-      peekFilter,
-      new provider.PeekFileDeclarationProvider(
-        declarationFilePatterns,
-        declarationMatchPattern
-      )
-    )
   );
 
   console.log("Extension activated");
 }
 
 export function deactivate() {
+  if (definitionProvider) {
+    definitionProvider.dispose();
+  }
+  if (implementationProvider) {
+    implementationProvider.dispose();
+  }
+  if (declarationProvider) {
+    declarationProvider.dispose();
+  }
+
   console.log("Extension deactivated");
 }
